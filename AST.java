2d85bbedb6ef4a081f934ac67ac65a7352ac0490
Carlos Teixeira <carlosmtxcmcmtx@gmail.com>
2014-04-07 17:14:24 +0100

    Do

---
+++ AST.java
@@ -0,0 +1,33 @@
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Vector;
+
+/**
+ * Created by Papa Formigas on 07-04-2014.
+ */
+public class AST{
+    Vector<String> expressions;
+    Vector<SimpleNode> trees;
+    Invariant parser;
+    public AST(){
+        trees = new Vector<SimpleNode>();
+        expressions = new Vector<String>();
+        parser=new Invariant(new ByteArrayInputStream("".getBytes()));
+
+    }
+
+        boolean parse(String string){
+            try{
+            parser.ReInit(new ByteArrayInputStream(string.getBytes()));
+            trees.add(parser.Inv_exp());
+            expressions.add(string);
+            return true;
+            }
+            catch(ParseException e){
+                return false;
+            }
+        }
+
+}
